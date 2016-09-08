91bbe475294deec0edd97f6d3803b3584ccf8991
maxahn <ahnmaximus@gmail.com>
2016-09-07 22:16:08 -0700

    set up

--- maze.js
+++ maze.js
@@ -1,69 +1,29 @@
-var wall = '\u2588';
-
-var empty = ' ';
-
-var width = process.argv[2];
+var WALL = '\u9641';
+var EMPTY  = ' ';
+var width = process.argv[2];  //default if there is no input 
 var height = process.argv[3];
 
-function generateMaze(width, height) {
-  var maze = [];
-  var mazeString = wall.repeat(width);
-  for (var i = 0; i < height; i++) {
-    maze.push(mazeString); 
-  } 
-  prettyPrint(mainPath(maze, width, height));
-}
+width = (width) ? width : 50;
+height = (height) ? height : 50;
 
-function mainPath(maze, width, height) {
-  var ranNum = Math.floor(Math.random() * 100) % width;
-  var firstMazeString = maze[0];
-  firstMazeString = firstMazeString.substring(0, ranNum - 1) + empty + firstMazeString.substring(ranNum, firstMazeString.length);
-  maze[0] = firstMazeString;
+var maze = [];
 
-  surroundingValidPos(ranNum - 1, 0);
-  canCarve(ranNum - 1, 0);
-  for (var i = 1; i < maze.length; i++) {
-    mazeString = maze[i];
+function generateEmpty(width, height) {
+  var mazeString = WALL.repeat(width);
+  for (var counter = 0; counter < height; counter++) {
+    maze.push(mazeString);
   }
-  return maze;
-}
+}  
 
-function surroundingValidPos(x, y) { //first time pass in 0,3 for example 
-  var moves = [];//.push(up).push(down).push(left).push(right);
-
-  (y >= 1) ? moves.push({dir: 'up', x: x, y: y - 1}) : 'blah'; 
-  (y <= height - 2) ? moves.push({dir: 'down', x: x, y: y + 1}) : 'blah'; 
-  (x >= 1) ? moves.push({dir: 'left', x: x - 1, y: y}) : 'blah';
-  (x <= width - 2) ? moves.push({dir: 'right', x:  x + 1, y: y}) : 'blah';
-  // for (var i = 0; i < moves.length; i++) { //printing out 
-  //   var move = moves[i];
-  //   console.log('dir: ' + move.dir + ' x: ' + move.x + ' y: ' + move.y);
-  // }
-  return moves;
-} 
-
-function canCarve(x, y) {
-  var moves = surroundingValidPos(x, y); 
-  var upValid = moves.find(function(move) {
-    return move.dir === 'up';
-  });
-  var downValid = moves.find(function(move) {
-    return move.dir === 'down'
-  });
-  var leftValid = moves.find(function(move) {
-    return move.dir === 'left'
-  });
-  var rightValid = moves.find(function(move) {
-    return move.dir === 'right'
-  });
-  return downValid !== undefined && upValid !== undefined && leftValid !== undefined && rightValid !== undefined;
-}
-
-
-function prettyPrint(maze) {
+function printMaze() {
   for (var counter = 0; counter < maze.length; counter++) {
     console.log(maze[counter]);
   }
 }
-// prettyPrint(generateMaze(width, height));
-generateMaze(width, height);
+
+function init() {
+  generateEmpty(width, height);
+  printMaze();
+}
+
+init();
